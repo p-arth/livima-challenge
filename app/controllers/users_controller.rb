bc88dfa532b3fb83eeaf170cbee141d598921f26
Pedro Arthur <pedro.art.vel@gmail.com>
2020-10-03 19:56:56 -0300

    Added new routes and user create/editing features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,38 @@
 class UsersController < ApplicationController
-  before_action :set_user, except: :update
+  before_action :set_user, except: [:index, :new, :create]
+
+  def index
+    @users = User.all
+  end
 
   def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find_by_id(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to users_path, notice: "Updated User."
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: "User succesfully created!" 
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -20,6 +45,10 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
     # authorize @user
+  end
+
+  def user_params
+    params.require(:user).permit(:nome, :email, :password, :password_confirmation, :cargo, :salário)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users, :path => 'auth', controllers: {confirmations: 'confirmations'}
 
-  resources :users, except: %i[index new create]
-  
+  resources :users, :controller => 'users'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
